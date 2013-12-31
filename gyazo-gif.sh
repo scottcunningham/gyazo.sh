6b901ba0653934afb07681d6f02d46c011fbb462
Scott Cunningham <you@example.com>
2013-12-31 20:00:24 +0000

    fixed gyazo-gif.sh to use system-wide gifshooter binary instead of ./gifshooter

--- gyazo-gif.sh
+++ gyazo-gif.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 fn=`tempfile`.gif
 echo $fn
-./gifshooter $fn
+gifshooter $fn
 url=$(curl -0 -X POST -F imagedata=@"$fn" http://img.roflol.pw/upload 2>/dev/null)
 echo $url
 xdg-open $url
