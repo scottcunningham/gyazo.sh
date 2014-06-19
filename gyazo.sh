e7febc54483971ebedcc3a6fce2a3bc86cbf75c0
Scott Cunningham <cunninsc@tcd.ie>
2014-06-19 14:46:48 +0100

    New hostname

--- gyazo.sh
+++ gyazo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 fn=`tempfile`.png
 gnome-screenshot -a -f $fn
-url=$(curl -0 -X POST -F imagedata=@"$fn" http://img.roflol.pw/upload 2>/dev/null)
+url=$(curl -0 -X POST -F imagedata=@"$fn" http://img.scottc.me/upload 2>/dev/null)
 echo $url
 xdg-open $url
 rm $fn
